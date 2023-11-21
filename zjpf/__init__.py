cc70ab1f1b9b62711d66a9a1f8c83f705fe4e57a
jzhang <18326675997@163.com>
2023-11-21 15:08:02 +0800

    update

--- zjpf/__init__.py
+++ zjpf/__init__.py
@@ -7,7 +7,7 @@ Created on Mon Nov 20 17:35:39 2023
 
 __version__ = '0.0.3'
 
-import np, math, RGI, os
+# import np, math, RGI, os
 from .f import *
 
 # remove redundant variables from the global memory.
